2f55b0425ead666ba6041e4f15685e56aac33b62
Van Tung Dinh <122477565+dinh-van-tung@users.noreply.github.com>
2025-06-25 14:05:49 +0700

    Create formcode.cpp

---
+++ formcode.cpp
@@ -0,0 +1,55 @@
+/*
+     ()_()                                ()_()
+    ( o.o )  -> *Author: VanTungDinh <-  ( -.- )
+     > ^ <                                (")(")
+     Link submit: None
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define pil pair<int, long long>
+#define pli pair<long long, int>
+#define pll pair<long long, long long>
+#define ff(i, a, b, j) for (int i = (a), bb = (b), jj = (j); i <= bb; i += jj)
+#define rf(i, a, b, j) for (int i = (a), bb = (b), jj = (j); i >= bb; i -= jj)
+#define lshift(x, i) ((x) << (i))
+#define rshift(x, i) ((x) >> (i))
+#define checkbit(x, i) (((x) >> (i)) & 1ll)
+#define cnt_bit1(x) __builtin_popcountll((x))
+#define clz(x) __builtin_clzll((x)) // count leading zeros
+#define ctz(x) __builtin_ctzll((x)) // count trailing zeros
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define db double
+#define pi acos(-1)
+#define maxn 1000005
+#define mod 1000000007
+
+void input(void) {
+}
+
+void solve(void) {
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int test_case = 1;
+//    cin >> test_case;
+    ff (i, 1, test_case, 1) {
+        input();
+        solve();
+    }
+    return 0;
+}
+/*
+     ()_()                                ()_()
+    ( o.o )  -> *Author: VanTungDinh <-  ( -.- )
+     > ^ <                                (")(")
+     Link submit: None
+*/
